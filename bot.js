e16ff9482dd46878810446dc689bea4bfd855579
IntriguingTiles <jnickw@outlook.com>
2017-12-31 01:40:50 -0700

    Initial implementation

---
+++ bot.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const fs = require("fs");
+const prefix = "h!";
+
+const client = new Discord.Client({ disableEveryone: true});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.username}`);
+    client.loadCommands();
+    client.user.setGame(`${prefix}help`);
+});
+
+client.on("message", async msg => {
+    if (!msg.content.startsWith(prefix)) return;
+    if (msg.author.bot) return;
+    if (msg.channel.type !== "text") return;
+
+    const args = msg.content.split(" ").slice(1);
+    const cmd = msg.content.slice(prefix.length).split(" ")[0];
+
+    if (cmd in client.commands) {
+        client.commands[cmd].run(client, msg, args);
+    }
+});
+
+client.loadCommands = () => {
+    const commands = fs.readdirSync("./commands/");
+    client.commands = {};
+    for (let i = 0; i < commands.length; i++) {
+        let cmd = commands[i];
+        if (cmd.match(/\.js$/)) {
+            delete require.cache[require.resolve(`./commands/${cmd}`)];
+            client.commands[cmd.slice(0, -3)] = require(`./commands/${cmd}`);
+            cmd = client.commands[cmd.slice(0, -3)];
+            if (cmd.aliases) {
+                for (let j = 0; j < cmd.aliases.length; j++) {
+                    client.commands[cmd.aliases[j]] = cmd;
+                }
+            }
+        }
+    }
+    console.log(`Loaded ${commands.length} commands!`);
+};
---
+++ commands/eval.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars*/
+const Discord = require("discord.js");
+const fs = require("fs");
+const {Client, Message} = require("discord.js");
+
+/**
+ * @param {Client} client
+ * @param {Message} msg
+ * @param {string[]} args
+ */
+exports.run = async(client, msg, args) => {
+    if (msg.author.id !== "221017760111656961") {
+        msg.reply(":no_entry: **I! WILL! KICK! YOUR! ASS!** :no_entry:");
+        return;
+    }
+
+    try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+
+        if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled);
+
+        msg.channel.send(clean(evaled), { code: "xl" });
+    } catch (err) {
+        msg.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+};
+
+function clean(text) {
+    if (typeof (text) === "string")
+        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
