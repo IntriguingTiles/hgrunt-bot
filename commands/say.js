dd8e314a8509d53034a9dfedc01e13c85c3fdff5
Jesse Walker <jnickw@outlook.com>
2017-12-31 16:50:53 -0700

    Added VOX support

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,8 @@ const tempMessage = require("../utils/tempmessage.js");
 const voice = require("../utils/voice.js");
 const { Client, Message } = require("discord.js"); // eslint-disable-line no-unused-vars
 
-const voiceLines = fs.readdirSync("./hgrunt").reverse();
+const hgruntVoiceLines = fs.readdirSync("./hgrunt").reverse();
+const voxVoiceLines = fs.readdirSync("./vox");
 
 /**
  * @param {Client} client
@@ -12,37 +13,54 @@ const voiceLines = fs.readdirSync("./hgrunt").reverse();
  */
 exports.run = async (client, msg, args) => {
     if (args.length > 0) {
-        if (!msg.member.voiceChannel) return tempMessage(msg.channel, "Join a voice channel first!", 5000);
-
-        const lines = ["clik.wav"];
-
-        for (let i = 0; i < args.length; i++) {
-            const arg = args[i];
-            let foundLine = false;
-
-            for (let j = 0; j < voiceLines.length; j++) {
-                const voiceLine = voiceLines[j];
-                
-                if (voiceLine.startsWith(arg.toLowerCase().replace(",", ""))) {
-                    // found the voice line
-                    lines.push(voiceLine);
-                    foundLine = true;
-                    break;
-                }
-            }
-
-            if (!foundLine) {
-                msg.react("❌");
-                tempMessage(msg.channel, `I couldn't find the word \`${arg}\` in my word list!`, 5000);
-                return;
-            }
-
-            if (arg.includes(",")) lines.push("_comma.wav");
+        if (args[0] === "vox") {
+            args.shift(); // remove vox from args
+            parse(msg, args, voxVoiceLines, "dadeda.wav");
+        } else {
+            parse(msg, args, hgruntVoiceLines, "clik.wav", "clik.wav");
         }
-        lines.push("clik.wav");
-        msg.react("👌");
-        voice.addLines(msg, lines);
     } else {
         tempMessage(msg.channel, "You did not provide any words for me to say!", 5000);
     }
 };
+
+/**
+ * @param {Message} msg
+ * @param {string[]} args 
+ * @param {string[]} voiceLines 
+ * @param {string} firstLine 
+ * @param {string} lastLine 
+ */
+function parse(msg, args, voiceLines, firstLine, lastLine) {
+    if (!msg.member.voiceChannel) return tempMessage(msg.channel, "Join a voice channel first!", 5000);
+
+    const lines = [firstLine];
+
+    for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+        let foundLine = false;
+
+        for (let j = 0; j < voiceLines.length; j++) {
+            const voiceLine = voiceLines[j];
+
+            if (voiceLine.startsWith(arg.toLowerCase().replace(",", ""))) {
+                // found the voice line
+                lines.push(voiceLine);
+                foundLine = true;
+                break;
+            }
+        }
+
+        if (!foundLine) {
+            msg.react("❌");
+            tempMessage(msg.channel, `I couldn't find the word \`${arg}\` in my word list!`, 5000);
+            return;
+        }
+
+        if (arg.includes(",")) lines.push("_comma.wav");
+    }
+
+    if (lastLine) lines.push(lastLine);
+    msg.react("👌");
+    voice.addLines(msg, lines);
+}
