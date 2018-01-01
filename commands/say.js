c6bc80a5997bc97cd0abaae4745f5e71b52a954e
Jesse Walker <jnickw@outlook.com>
2017-12-31 18:43:01 -0700

    Lower word limit

--- commands/say.js
+++ commands/say.js
@@ -16,7 +16,7 @@ const rateLimitedUsers = new Map();
  * @param {string[]} args
  */
 exports.run = async (client, msg, args) => {
-    if (args.length >= 20) return tempMessage(msg.channel, "Too many words!", 5000);
+    if (args.length >= 10) return tempMessage(msg.channel, "Too many words!", 5000);
     if (args.length > 0) {
         if (args[0] === "vox") {
             if (args.length > 2) return tempMessage(msg.channel, "```Usage: h!say [vox] <words>```", 5000);
