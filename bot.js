0127946363c553d94bbd8d80b9240c5a060f39c8
Jesse Walker <jnickw@outlook.com>
2018-01-08 14:11:01 -0700

    Prepare for guild specific configuration

--- bot.js
+++ bot.js
@@ -1,26 +1,40 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const fs = require("fs");
-const prefix = "!";
+const Enmap = require("enmap");
+const EnmapLevel = require("enmap-level");
 
 const client = new Discord.Client({ disableEveryone: true});
+client.guildSettings = new Enmap({provider: new EnmapLevel({name: "guildSettings"})});
+
+const defaultSettings = {
+    prefix: "!", // command prefix
+    limits: true // should we enable limits
+};
 
 client.login(process.env.DISCORD_TOKEN);
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.username}`);
     client.loadCommands();
-    client.user.setGame(`${prefix}say`);
+    client.user.setGame("!say");
+    // now that the prefix could change on a per-guild basis, what do we do here? :thinking:
+    // for now, we'll leave it as the default prefix.
+});
+
+client.on("guildCreate", async guild => {
+    client.guildSettings.set(guild.id, defaultSettings);
 });
 
 client.on("message", async msg => {
+    const guildSettings = client.guildSettings.get(msg.guild.id);
     if (msg.guild.id === 154305477323390976 && msg.channel.id !== 297518974730764288) return; // only do things in #bot-commands in the HL Discord server
-    if (!msg.content.startsWith(prefix)) return;
+    if (!msg.content.startsWith(guildSettings.prefix)) return;
     if (msg.author.bot) return;
-    if (msg.channel.type !== "text") return;
+    if (msg.channel.type !== "text") return; // only do things in a text channel
 
     const args = msg.content.split(" ").slice(1);
-    const cmd = msg.content.slice(prefix.length).split(" ")[0];
+    const cmd = msg.content.slice(guildSettings.prefix.length).split(" ")[0];
 
     if (cmd in client.commands) {
         client.commands[cmd].run(client, msg, args);
