a06a85a849cebbc87648cf280ea2e076bf592388
IntriguingTiles <jnickw@outlook.com>
2018-02-12 16:26:20 -0700

    Add hdtf command

---
+++ commands/hdtf.js
@@ -0,0 +1,40 @@
+const Jimp = require("jimp");
+const { Client, Message, Attachment } = require("discord.js"); // eslint-disable-line no-unused-vars
+
+/**
+ * @param {Client} client
+ * @param {Message} msg
+ * @param {string[]} args
+ */
+exports.run = async (client, msg, args) => {
+    if (args.length <= 0) return msg.channel.send("```Usage: !hdtf <words>```");
+
+    let realLength = 0;
+
+    for (let i = 0; i < args.length; i++) realLength += args[i].length;
+
+    const wordsImageWidth = (realLength + args.length) * 34 - 34;
+    const wordsImageHeight = 44;
+
+    const wordsImage = new Jimp(wordsImageWidth, wordsImageHeight, async function (err, image) {
+        if (err) return msg.channel.send("Failed to create image!");
+        let currentX = 0;
+
+        for (let i = 0; i < args.length; i++) {
+            for (let j = 0; j < args[i].length; j++) {
+                const char = args[i].charAt(j);
+                const letter = await Jimp.read(`./hdtf/${char}.png`);
+                wordsImage.composite(letter, currentX, 0);
+                currentX += 34;
+            }
+            currentX += 34;
+        }
+        const bannerImage = await Jimp.read("./hdtf/banner.png");
+        wordsImage.resize(bannerImage.bitmap.width, 44, Jimp.RESIZE_NEAREST_NEIGHBOR);
+        bannerImage.composite(wordsImage, 0, bannerImage.bitmap.height - 44);
+        bannerImage.getBuffer(Jimp.AUTO, function (err, buffer) {
+            msg.channel.send(new Attachment(buffer, "hdtf.png"));
+            msg.channel.stopTyping();
+        });
+    });
+};
