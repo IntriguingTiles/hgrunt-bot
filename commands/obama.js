82f2f66daa7d019d64aed2a3f48627bb1fd5d966
IntriguingTiles <jnickw@outlook.com>
2018-10-05 21:12:51 -0600

    temp change

--- commands/obama.js
+++ commands/obama.js
@@ -27,7 +27,7 @@ exports.run = async (client, msg, args) => {
         try {
             request = await snekfetch.post("http://talkobamato.me/synthesize.py", { redirect: false }).attach("input_text", words);
         } catch (err) {
-            msg.channel.send("Failed to create video! Probably an issue with your input.");
+            msg.channel.send("Obama video generator is currently broken.");
             return msg.channel.stopTyping();
         }
         //console.log(request.headers.location);
