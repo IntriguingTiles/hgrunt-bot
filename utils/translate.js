1c97f10445f3d62e67410794847476a3a7a51f80
IntriguingTiles <jnickw@outlook.com>
2019-12-11 12:55:12 -0700

    Hack to let YGrunt speak again

--- utils/translate.js
+++ utils/translate.js
@@ -5,9 +5,8 @@ const Entities = require("html-entities").XmlEntities;
 const entities = new Entities();
 
 module.exports = async text => {
-    const r = await snekfetch.post("http://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl").set("Content-Type", "text/xml")
-        .send(`<Envelope><Body><yodaTalk><yodaTalkRequest>${entities.encode(text)}</yodaTalkRequest></yodaTalk></Body></Envelope>`);
-    const $ = cheerio.load(r.body);
+    const r = await snekfetch.post("http://www.yodaspeak.co.uk/index.php").set("Content-Type", "application/x-www-form-urlencoded").set("Referer", "http://www.yodaspeak.co.uk/").attach("YodaMe", entities.encode(text));
 
-    return entities.decode($("return").text());
+const $ = cheerio.load(r.body);
+    return entities.decode($("textarea[name='YodaSpeak']").text());
 };
