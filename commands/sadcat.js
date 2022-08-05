7d3e4def085f190aecdb00d266140ea619bec1ac
IntriguingTiles <jnickw@outlook.com>
2022-08-05 15:47:11 -0600

    Commands: SadCat: Defer everything

--- commands/sadcat.js
+++ commands/sadcat.js
@@ -38,13 +38,15 @@ exports.aliases = ["sc"];
  * @param {ChatInputCommandInteraction} intr
  */
 exports.run = async (client, intr, guildSettings) => {
+    await intr.deferReply({ ephemeral: guildSettings.ephemeral });
+
     switch (intr.options.getSubcommand()) {
         case "user":
-            return sendImage(intr.options.getUser("user").displayAvatarURL({ extension: "png", size: 512 }), intr, guildSettings);
+            return sendImage(intr.options.getUser("user").displayAvatarURL({ extension: "png", size: 512 }), intr);
         case "image":
-            return sendImage(intr.options.getAttachment("image").url, intr, guildSettings);
+            return sendImage(intr.options.getAttachment("image").url, intr);
         case "url":
-            return sendImage(intr.options.getString("url"), intr, guildSettings);
+            return sendImage(intr.options.getString("url"), intr);
     }
 };
 
@@ -52,7 +54,7 @@ exports.run = async (client, intr, guildSettings) => {
  * @param {string} url 
  * @param {ChatInputCommandInteraction} intr
  */
-async function sendImage(url, intr, guildSettings) {
+async function sendImage(url, intr) {
     try {
         const finalImg = new Jimp(720, 960, 0x000000FF); // new black image
         const sadCat = await Jimp.read("./sadcat.png");
@@ -60,9 +62,9 @@ async function sendImage(url, intr, guildSettings) {
         img.scaleToFit(501, 382);
         finalImg.composite(img, 0, 207);
         finalImg.composite(sadCat, 0, 0);
-        await intr.reply({ files: [await finalImg.getBufferAsync(Jimp.AUTO)], ephemeral: guildSettings.ephemeral });
+        await intr.editReply({ files: [await finalImg.getBufferAsync(Jimp.AUTO)] });
     } catch (err) {
-        intr.reply({ content: "Bad URL or not an image!", ephemeral: true });
+        intr.editReply("Bad URL or not an image!");
     }
 
 }
